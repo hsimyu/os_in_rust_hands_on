b77708bfc30c7061609336e7d5d376e379fbff21
hsimyu <hsimyu1900@gmail.com>
2023-12-29 19:56:45 +0900

    Print panic! message.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
