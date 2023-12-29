0cf554a60267f0c5418093c899b8080992b39a4c
hsimyu <hsimyu1900@gmail.com>
2023-12-29 19:29:15 +0900

    Impl new_line().

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -75,7 +75,30 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                // 改行があったらここまでのテキストを一行上にコピーする
+                // (効率悪くない?)
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 use core::fmt;
