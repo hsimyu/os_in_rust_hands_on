43e8f7d0550a9fecd95620f0eff1e4a006120370
hsimyu <hsimyu1900@gmail.com>
2023-12-30 16:53:27 +0900

    Add qemu exit mapping.

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,28 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
