87ae72f75fc931e947a38a9193d776d5dca5631e
hsimyu <hsimyu1900@gmail.com>
2024-01-01 18:17:06 +0900

    Impl double fault handler.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,10 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
     blog_os::init();
-    x86_64::instructions::interrupts::int3();
+
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    }
 
     #[cfg(test)]
     test_main();
