2598f22f7b186daa5001c2d532fb7197281be8d5
hsimyu <hsimyu1900@gmail.com>
2024-01-03 20:24:27 +0900

    Impl virtual address translation.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(blog_os::test_runner)]
 
-use blog_os::println;
+use blog_os::{memory::translate_addr, println};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::VirtAddr;
 
 entry_point!(kernel_main);
 
@@ -14,6 +15,21 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     blog_os::init();
+
+    // L4 ページテーブルへアクセス
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("virt = {:?} -> phys = {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
