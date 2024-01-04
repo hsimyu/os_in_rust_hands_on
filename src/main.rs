c066485f6a70599cc38a9dc2b3749c55178c6032
hsimyu <hsimyu1900@gmail.com>
2024-01-04 20:52:08 +0900

    Impl EmtptyFrameAllocator.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 use blog_os::println;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use x86_64::{structures::paging::Translate, VirtAddr};
+use x86_64::{structures::paging::Page, VirtAddr};
 
 entry_point!(kernel_main);
 
@@ -20,19 +20,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     // L4 ページテーブルへアクセス
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAlocator;
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        // let phys = unsafe { translate_addr(virt, phys_mem_offset) };
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
+    // 未使用のページをマップする
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // 新しいマッピングを使って、文字列を書き出す
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe {
+        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
     }
 
     #[cfg(test)]
