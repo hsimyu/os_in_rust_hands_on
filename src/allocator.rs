ff6a119cd20c173bb7f39cae8626f50c935da0f9
hsimyu <hsimyu1900@gmail.com>
2024-01-06 23:17:18 +0900

    Impl page mapping for global allocator.

--- src/allocator.rs
+++ src/allocator.rs
@@ -3,6 +3,16 @@ use core::{
     ptr::null_mut,
 };
 
+use x86_64::{
+    structures::paging::{
+        mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
+    },
+    VirtAddr,
+};
+
+pub const HEAP_START: usize = 0x_4444_4444_0000; // 適当な仮想アドレス
+pub const HEAP_SIZE: usize = 100 * 1024;
+
 pub struct Dummy;
 
 #[global_allocator]
@@ -16,4 +26,32 @@ unsafe impl GlobalAlloc for Dummy {
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
         panic!("dealloc should be never called")
     }
+}
+
+pub fn init_heap(
+    mapper: &mut impl Mapper<Size4KiB>,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) -> Result<(), MapToError<Size4KiB>> {
+    let page_range = {
+        let heap_start = VirtAddr::new(HEAP_START as u64);
+        // NOTE: heap_end をヒープの最後の有効なバイトのアドレスにしたいので、-1 する
+        let heap_end = heap_start + HEAP_SIZE - 1u64;
+        let heap_start_page = Page::containing_address(heap_start);
+        let heap_end_page = Page::containing_address(heap_end);
+        Page::range_inclusive(heap_start_page, heap_end_page)
+    };
+
+    // 作成したページ範囲内にフレームをマッピングする
+    for page in page_range {
+        let frame = frame_allocator
+            .allocate_frame()
+            .ok_or(MapToError::FrameAllocationFailed)?;
+
+        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+
+        // NOTE: flush() することで TLB を明示的に更新する
+        unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,33 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(blog_os::test_runner)]
 
-use blog_os::println;
+extern crate alloc;
+
+use alloc::boxed::Box;
+use blog_os::{
+    allocator,
+    memory::{self, BootInfoFrameAllocator},
+    println,
+};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::VirtAddr;
 
 entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     blog_os::init();
 
     // L4 ページテーブルへアクセス
-    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    // let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    // let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let x = Box::new(41);
 
     #[cfg(test)]
     test_main();
