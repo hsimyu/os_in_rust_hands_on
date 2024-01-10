882ee317a1c11c99db5843763d88b400d52e89b8
hsimyu <hsimyu1900@gmail.com>
2024-01-11 00:06:37 +0900

    Impl run() method.

--- src/task/simple_executor.rs
+++ src/task/simple_executor.rs
@@ -1,4 +1,4 @@
-use core::task::{RawWaker, Waker};
+use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 
 use alloc::collections::VecDeque;
 
@@ -18,10 +18,29 @@ impl SimpleExecutor {
     pub fn spawn(&mut self, task: Task) {
         self.task_queue.push_back(task)
     }
+
+    pub fn run(&mut self) {
+        while let Some(mut task) = self.task_queue.pop_front() {
+            let waker = dummy_waker();
+            let mut context = Context::from_waker(&waker);
+            match task.poll(&mut context) {
+                // タスク完了
+                Poll::Ready(()) => {}
+                // タスクを積み直す
+                Poll::Pending => self.task_queue.push_back(task),
+            }
+        }
+    }
 }
 
 fn dummy_raw_waker() -> RawWaker {
-    todo!();
+    fn no_op(_: *const ()) {}
+    fn clone(_: *const ()) -> RawWaker {
+        dummy_raw_waker()
+    }
+
+    let vtable = &RawWakerVTable::new(clone, no_op, no_op, no_op);
+    RawWaker::new(0 as *const (), vtable)
 }
 
 fn dummy_waker() -> Waker {
